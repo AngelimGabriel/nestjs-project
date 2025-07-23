40a5f58ad930ec1a5677d53c652118f5de33953e
Gabriel Angelim <gabriel_angelim@outlook.com>
2025-07-23 17:25:19 -0300

    Correção no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AddressModule } from './address/address.module';
 import { PrismaModule } from 'prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AddressModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UsersModule,
+    PrismaModule,
+    AddressModule,
+  ],
 })
 export class AppModule {}
